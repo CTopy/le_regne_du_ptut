9dea890ed82fb11764f69484b640293da1deed3b
Unknown <Clement.Orlandini.Etu@univ-lemans.fr>
2018-10-25 14:40:35 +0200

    Ajout éléments graphiques 2

--- js/Player.js
+++ js/Player.js
@@ -10,14 +10,11 @@ Player = function(game, canvas) {
 Player.prototype = {
     _initCamera : function(scene, canvas) {
         // On crée la caméra
-        this.camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 30, new BABYLON.Vector3(0, 0, 0), scene);
+        this.camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 30, new BABYLON.Vector3(0, 20, -50), scene);
         
 
         // On demande à la caméra de regarder au point zéro de la scène
         this.camera.setTarget(BABYLON.Vector3.Zero());
-        
-        //Placer la caméra dans l'espace
-        this.camera.setPosition(new BABYLON.Vector3(30, 30, 0));
 
         // On affecte le mouvement de la caméra au canvas
         this.camera.attachControl(canvas, true);
