cf458a94c6ecc340c6b8832119920e732de83c91
BroZ <ludovicwarin53@gmail.com>
2018-11-26 15:58:56 +0100

    BaseCodeDiversV3
    Cultiste/investiguateur/grandAncien/shoggoth

--- js/Jeu/Shoggoth.js
+++ js/Jeu/Shoggoth.js
@@ -16,4 +16,12 @@ class Shoggoth{
     mourir(){
         this.destroy();
     }
+    
+    seDeplacer(){
+        var destination = this.lieu.versPortail;
+        var coordoneeDestination = destination.ajouterEntite(this);
+        //animation?
+        //var animationMvnt = new BABYLON.animation("animationInvestigateur", "position", 30,BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+        this.lieu=coordoneeDestination;
+    }
 }
