bc944a5e0610fb1bb4f52a2a7cbefe142333505c
BroZ <ludovicwarin53@gmail.com>
2018-11-26 15:03:09 +0100

    BaseCodeDiversV1
    base du code des investiguateur, de grands anciens et des cultistes

---
+++ js/Jeu/Cultiste.js
@@ -0,0 +1,19 @@
+"use strict";
+
+class Cultiste{
+    
+    constructor(lieu){
+        this.lieu = lieu;
+        var coordoneeDestination = lieu.ajouterEntite(this);
+        
+        this.pion = BABYLON.SceneLoader.ImportMesh("", "", "./assets/modeles/cultiste.babylon", scene, function(newMeshes) {
+            newMeshes.forEach(function(mesh){
+               mesh.position = coordoneeDestination;
+            });
+        });
+    }
+    
+    mourir(){
+        this.destroy();
+    }
+}
