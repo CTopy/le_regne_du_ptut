9dfde5fee73d0b5853933d510cb544f32be783eb
BroZ <ludovicwarin53@gmail.com>
2018-11-26 11:46:13 +0100

    InvestigateurV3
    Changement de redaction des classes. Début de travail sur la methode marcher

--- js/Jeu/Investigateur.js
+++ js/Jeu/Investigateur.js
@@ -1,71 +1,72 @@
 "use strict"
 
-function Investigateur(joueur){
-    var nbAction;
-    var santeMentale;
-    var nbCarteEnMainPutainDeMagicien;
-    var effet;
-    var image;
-    var estFou;
+class Investigateur{
+    constructor(joueur){
+        this.joueur=joueur;
+        this.nbAction = 4;
+        this.santeMentale = 4;
+        this.effet="";
+        this.image="";
+        this.estFou=false;
+        this.lieu=GARE;
+    }
     
-    this.joueur=joueur;
-    this.nbAction = 4;
-    this.santeMentale = 4;
-    this.effet="";
-    this.image="";
-    this.estFou=false;
-    
-    this.devenirFou = function(){
+    devenirFou() {
         this.estFou=true;
         this.nbAction = 3;
         this.effet = "";
         this.image = "";
     }
-    this.seRetablirDeFolie = function(){
+    seRetablirDeFolie(){
         this.estFou=false;
         this.nbAction = 3;
         this.effet = "";
         this.image = "";
     }
+    
+    marcher(destination){
+        var coordoneeDestination = destination.ajouterEntite;
+        this.lieu=coordoneeDestination;
+    }
 }
 
-var Detective = function(joueur){
-    Investigateur.call(joueur);
-    this.joueur=joueur;
-    this.personnage="Détective";
-    this.nbAction = 4;
-    this.santeMentale = 4;
-    this.nbCarteEnMainPutainDeMagicien = 7;
-    this.effet="Vous n'avez besoin que de 4 cartes de la même couleur pour sceller un portail."
-    this.image="./images/detective.jpg";
-    this.estFou=false;
+class Detective{
+    constructor(joueur){
+        super(joueur);
+        this.personnage="Détective";
+        this.nbAction = 4;
+        this.santeMentale = 4;
+        this.nbCarteEnMainPutainDeMagicien = 7;
+        this.effet="Vous n'avez besoin que de 4 cartes de la même couleur pour sceller un portail."
+        this.image="./images/detective.jpg";
+        this.estFou=false;
+    }
     
-    this.devenirFou = function(){
+    devenirFou(){
         this.estFou=true;
         this.nbAction = 3;
         this.effet = "Vous n'avez besoin que de 4 cartes de la même couleur pour sceller un portail. De plus, si vous participez à l'action prendre ou donner une carte indice, cela coute 2 actions au joueur actif";
         this.image = "./images/detectiveFou.jpg";
     }
-    this.seRetablirDeFolie = function(){
+    seRetablirDeFolie(){
         this.estFou=false;
         this.nbAction = 4;
         this.effet="Vous n'avez besoin que de 4 cartes de la même couleur pour sceller un portail."
         this.image="./images/detective.jpg";
     }
-    
-    this.prototype = new Investigateur();
 }
 
-var Docteur = function(joueur){
-    Investigateur.call(joueur);
-    this.joueur=joueur;
-    this.personnage="Docteur";
-    this.nbAction = 5;
-    this.santeMentale = 4;
-    this.nbCarteEnMainPutainDeMagicien = 7;
-    this.effet="Vous pouvez effectuer jusqu'à 5 actions par tour."
-    this.image="./images/docteur.jpg";
-    this.estFou=false;
+class Docteur{
+    constructor(joueur){
+        super(joueur);
+        this.personnage="Docteur";
+        this.nbAction = 5;
+        this.santeMentale = 4;
+        this.nbCarteEnMainPutainDeMagicien = 7;
+        this.effet="Vous pouvez effectuer jusqu'à 5 actions par tour."
+        this.image="./images/docteur.jpg";
+        this.estFou=false;
+    }
     
     this.devenirFou = function(){
         this.estFou=true;
@@ -79,21 +80,19 @@ var Docteur = function(joueur){
         this.effet="Vous pouvez effectuer jusqu'à 5 actions par tour."
         this.image="./images/docteur.jpg";
     }
-    
-    
-    this.prototype = new Investigateur();
 }
 
-var Conducteur = function(joueur){
-    Investigateur.call(joueur);
-    this.joueur=joueur;
-    this.personnage="Conducteur";
-    this.nbAction = 4;
-    this.santeMentale = 4;
-    this.nbCarteEnMainPutainDeMagicien = 7;
-    this.effet="Lors de l'action marcher, vous POUVEZ vous déplacer de 2 lieux au lieu de 1. De plus, vous êtes immunisé contre les effets d'Ithaqua."
-    this.image="./images/conducteur.jpg";
-    this.estFou=false;
+class Docteur{
+    constructor(joueur){
+        super(joueur);
+        this.personnage="Conducteur";
+        this.nbAction = 4;
+        this.santeMentale = 4;
+        this.nbCarteEnMainPutainDeMagicien = 7;
+        this.effet="Lors de l'action marcher, vous POUVEZ vous déplacer de 2 lieux au lieu de 1. De plus, vous êtes immunisé contre les effets d'Ithaqua."
+        this.image="./images/conducteur.jpg";
+        this.estFou=false;
+    }
     
     this.devenirFou = function(){
         this.estFou=true;
@@ -107,6 +106,4 @@ var Conducteur = function(joueur){
         this.effet="Lors de l'action marcher, vous POUVEZ vous déplacer de 2 lieux au lieu de 1. De plus, vous êtes immunisé contre les effets d'Ithaqua."
         this.image="./images/conducteur.jpg";
     }
-    
-    this.prototype = new Investigateur();
 }
