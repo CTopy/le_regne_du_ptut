2e214ce3264bf4b7fb22af0bec525698d4800320
Unknown <romain.lalune@yahoo.fr>
2018-11-26 15:41:03 +0100

    ActionV3

--- js/Jeu/Action.js
+++ js/Jeu/Action.js
@@ -96,3 +96,62 @@ class VaincreCultiste extends Action {
         }
     }
 }
+
+class VaincreShoggoth extends Action {
+    constructor (investigateur) {
+        this.nom = "Vaincre un Shoggoth";
+        super(investigateur);
+        this.entites = investigateur.lieu.entité;
+    }
+    
+    preaction() {
+        //on créer une div pour le slider
+        var newDiv = document.createElement("div");
+        //on y ajoute des boutons cliquables pour sélectionner le nombre de shoggoths à vaincre
+        var btn1 = document.createElement("button");
+        var textBtn1 = document.createTextNode("1");
+        newDiv.appendChild(btn1);
+        btn1.appendChild(textBtn1);
+        var btn2 = document.createElement("button");
+        var textBtn2 = document.createTextNode("2");
+        newDiv.appendChild(btn2);
+        btn2.appendChild(textBtn2);
+        var btn3 = document.createElement("button");
+        var textBtn3 = document.createTextNode("3");
+        newDiv.appendChild(btn3);
+        btn3.appendChild(textBtn3);
+        //on insère le tout dans la page
+        var iconeClique = document.getElementById("vaincreShog");
+        document.body.insertBefore(newDiv, iconeClique);
+        
+        //on ajoute des classes pour le positionnement en CSS
+        newDiv.id = "divVaincShog";
+        btn1.className = "btnVaincShog";
+        btn2.className = "btnVaincShog";
+        btn3.className = "btnVaincShog";
+        //on y met les écouteurs pour tuer les shoggoths
+        btn1.addEventListener("click", faireAction(1));
+        btn2.addEventListener("click", faireAction(2));
+        btn3.addEventListener("click", faireAction(3));
+    }
+    
+    faireAction(nbShogAVaincre) {
+        var newDiv = document.getElementById("divVaincShog");
+        document.body.removeChild(newDiv);
+        if(investigateur.nbActions<nbShogAVaincre) {
+            //si le nombre d'action du joueur est inférieur au nombre de Shoggoths à vaincre, on lui dit qu'il ne peut pas les tuer
+            alert("Vous n'avez pas suffisamment d'actions restantes");
+            preaction();
+        } else {
+            var shogAVaincre;
+            for(var i=0; i<nbShogAVaincre; i++) {
+                for(var uneEntite of entites) {
+                    if (typeof uneEntite === typeof new Shoggoth()) {
+                        uneEntite.mourir();
+                        this.investigateur.lieu.retirerEntite(uneEntite);
+                    }
+                }
+            }
+        }
+    }
+}
