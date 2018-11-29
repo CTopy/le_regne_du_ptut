a0757fa312369feb1401cb2a75ba418dde86d2ab
BroZ <ludovicwarin53@gmail.com>
2018-11-29 10:24:29 +0100

    finalisationChargement
    il ne manque que un peu de graphisme

--- js/chargement.js
+++ js/chargement.js
@@ -1,46 +1,63 @@
 "use strict"
 
+function afficherChargement(){
+    let fond = document.createElement("section");
+    let cadre = document.createElement("div");
+    let text = document.createElement("p");
+    let imageAnime = document.createElement("img");
 
-let fond = document.createElement("section");
-$(fond).css({
-    "width":"100vw",
-    "height":"100vh",
-    "background-color":"black",
-    "z-index":"100",
-    "display":"flex",
-    "justify-content":"center"
-});
-document.querySelector("body").prepend(fond);
 
-let cadre = document.createElement("div");
-$(cadre).css({
-    "width" : "30vw",
-    "height" : "30vh",
-    "border-radius" : "10px",
-    "margin" : "auto",
-    "position" : "absolute",
-    "top" : "20%",
-    "align-items": "center",
-    "padding" : "5%"
-   /* "src": "./assets/images/rectangle_chargement.png"*/
-});
-fond.append(cadre);
+    $(fond).css({
+        "width":"100vw",
+        "height":"100vh",
+        "background-color":"#021c05",
+        "z-index":"1000",
+        "position":"absolute",
+        "display":"flex",
+        "justify-content":"center"
+    });
+    document.querySelector("body").prepend(fond);
 
-let text = document.createElement("P");
-text.textContent="CHARGEMENT"
-$(text).css({
-    "text-align" : "center",
-    "font-size" : "5vw"
-});
-cadre.append(text);
+    $(cadre).css({
+        "width" : "30vw",
+        "height" : "30vh",
+        "border-radius" : "10px",
+        "margin" : "auto",
+        "position" : "absolute",
+        "display" : "flex",
+        "justify-content" : "space-around",
+        "top" : "20%",
+        "align-items": "center",
+        "padding" : "5%",
+        "src": "./assets/images/rectangle_chargement.png"
+    });
+    fond.append(cadre);
 
-let imageAnime = document.createElement("img");
-$(imageAnime).attr({
-    src : "./assets/images/chargement.png"
-});
-$(imageAnime).css({
-    "animation":"1.5s infinite cubic-bezier(.39, .01, .63, .99) TOURNE",
-    "margin":"auto",
-    "height":"50%"
-});
-cadre.append(imageAnime);
+    text.textContent="CHARGEMENT"
+    $(text).css({
+        "text-align" : "center",
+        "font-size" : "5vw"
+    });
+    cadre.append(text);
+
+    $(imageAnime).attr({
+        src : "./assets/images/chargement.png"
+    });
+    $(imageAnime).css({
+        "animation":"0.6s infinite linear TOURNE",
+        "margin":"auto",
+        "height":"50%"
+    });
+    cadre.append(imageAnime);
+}
+
+function effacerChargement(fond){
+    
+    $(fond).animate({
+        "opacity" : "0"
+    }, 2000, function() {
+        $(fond).empty();
+        $(fond).remove();
+    });
+}
+    
