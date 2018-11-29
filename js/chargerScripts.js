9a92d25931f111af931a480f61511b834e475d42
Topy <Clement.Orlandini.Etu@univ-lemans.fr>
2018-11-29 10:46:03 +0100

    Chargement fonctionnel

--- js/chargerScripts.js
+++ js/chargerScripts.js
@@ -9,11 +9,12 @@
  * 	});
  * });
  */
-
+var chgt= afficherChargement();
 $(document).ready(function() {
     console.log("Chargement en cours...");
     sssl(['js/Jeu/constantes.js', 'js/general.js', 'js/Arena.js', 'js/Player.js', 'js/Game.js', 'js/Jeu/Lieu.js', 'js/Jeu/Carte.js', 'js/Jeu/Deck.js', 'js/Jeu/Investigateur.js', 'js/Jeu/Cultiste.js', "js/Jeu/Shoggoth.js", "js/Jeu/GrandAncien.js", 'js/Jeu/Action.js', 'js/Vue.js', 'js/Jeu/Jeu.js'], function() {
         console.log("Fin du chargement !");
+        effacerChargement(chgt);
     });
 });
 
@@ -53,3 +54,66 @@ $(document).ready(function() {
 		});
 	};
 })();
+
+function afficherChargement(){
+    let fond = document.createElement("section");
+    let cadre = document.createElement("div");
+    let text = document.createElement("p");
+    let imageAnime = document.createElement("img");
+
+
+    $(fond).css({
+        "width":"100vw",
+        "height":"100vh",
+        "background-color":"#021c05",
+        "z-index":"1000",
+        "position":"absolute",
+        "display":"flex",
+        "justify-content":"center"
+    });
+    document.querySelector("body").prepend(fond);
+
+    $(cadre).css({
+        "width" : "30vw",
+        "height" : "30vh",
+        "border-radius" : "10px",
+        "margin" : "auto",
+        "position" : "absolute",
+        "display" : "flex",
+        "justify-content" : "space-around",
+        "top" : "20%",
+        "align-items": "center",
+        "padding" : "5%",
+        "src": "./assets/images/rectangle_chargement.png"
+    });
+    fond.append(cadre);
+
+    text.textContent="CHARGEMENT"
+    $(text).css({
+        "text-align" : "center",
+        "font-size" : "5vw",
+        "font-family" : "Tahoma"
+    });
+    cadre.append(text);
+
+    $(imageAnime).attr({
+        src : "./assets/images/chargement.png"
+    });
+    $(imageAnime).css({
+        "animation":"0.6s infinite linear TOURNE",
+        "margin":"auto",
+        "height":"50%"
+    });
+    cadre.append(imageAnime);
+    return fond;
+}
+
+function effacerChargement(fond){
+    
+    $(fond).animate({
+        "opacity" : "0"
+    }, 2000, function() {
+        $(fond).empty();
+        $(fond).remove();
+    });
+}
